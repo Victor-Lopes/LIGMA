2eb18a31dda800fa877a8cc108efbd644ce5094f
YremeMamoru <37820903+YremeMamoru@users.noreply.github.com>
2019-05-26 17:52:00 -0300

    Update LigmaProcedures.sql

--- ESaiaDeMansinho/LigmaProcedures.sql
+++ ESaiaDeMansinho/LigmaProcedures.sql
@@ -11,7 +11,7 @@ BEGIN
 	IF @Email IS NULL AND @Senha IS NULL AND @CEP <> 0AND @Numero <> 0 AND @Nome IS NULL AND
 						@Complemento IS NULL AND @Cidade IS NULL AND @UF IS NULL AND @Logradouro IS NULL AND
 						@RG IS NULL AND @CPF <> 0 AND @DataNasc IS NULL and @Salario IS NULL AND @Cargo IS NULL
-		SELECT 'Há atributos vazios' AS [ERRO];
+		SELECT 'HÃ¡ atributos vazios' AS [ERRO];
 	ELSE
 		BEGIN
 			INSERT INTO tbLogin(Email,Senha) VALUES (@Email, @Senha);
@@ -36,7 +36,7 @@ begin
 	declare @Mensagem varchar(40);
 	if @CodDiaSemana is null SELECT @Mensagem = 'Selecione pelo menos um dia da semana!'; 
 	else if @NomePeriodo is not null and exists(select NomePeriodo from tbPeriodo where NomePeriodo = @NomePeriodo)
-	SELECT @Mensagem = 'Nome Já Existente!'
+	SELECT @Mensagem = 'Nome JÃ¡ Existente!'
 	else
 	begin
 		declare @Dia1 varchar(8) = (SELECT Nome from tbDiaSemana where CodDiaSemana = @CodDiaSemana);
@@ -48,7 +48,7 @@ begin
 		else SELECT @NomePeriodo = @Dia1 +', '+ @Dia2 +', '+ @Dia3;
 
 		if @NomePeriodo is not null and exists(select NomePeriodo from tbPeriodo where NomePeriodo = @NomePeriodo) 
-		SELECT @Mensagem = 'Você já tem um periodo semelhante!';
+		SELECT @Mensagem = 'VocÃª jÃ¡ tem um periodo semelhante!';
 		else
 		begin
 			insert into tbPeriodo(NomePeriodo) values (@NomePeriodo);
@@ -64,6 +64,40 @@ begin
 	SELECT @Mensagem as Mensagem;
 end
 GO
+
+-- CADASTRAR PROFESSOR
+CREATE PROC cadProf  @Email VARCHAR(50), @Senha varchar(20), @CEP char(8), @Numero smallint, @Nome varchar(75),
+						@Complemento varchar(30) = '', @Cidade varchar(25), @UF char(2), @Logradouro varchar(140),
+						@RG char(10), @CPF decimal(11), @DataNasc date, @Salario money, @Cargo varchar(20), @Idioma varchar(18),
+						@TelCelular decimal(9) = 0, @TelFixo decimal(8) = 0
+AS
+BEGIN
+	IF @Email IS NULL AND @Senha IS NULL AND @CEP <> 0AND @Numero <> 0 AND @Nome IS NULL AND 
+		@Cidade IS NULL AND @UF IS NULL AND @Logradouro IS NULL AND
+		@RG IS NULL AND @CPF <> 0 AND @DataNasc IS NULL and @Salario IS NULL AND @Cargo IS NULL
+		AND @Idioma IS NULL
+		SELECT 'HÃ¡ atributos vazios' AS [ERRO];
+	ELSE
+		BEGIN
+			INSERT INTO tbLogin(Email,Senha) VALUES (@Email, @Senha);
+
+			INSERT INTO tbDadosComuns(CEP, Numero, Nome, Complemento, Cidade, UF, Logradouro, RG, CPF, DataNasc)
+			VALUES (@CEP, @Numero, @Nome, @Complemento, @Cidade, @UF, @Logradouro, @RG, @CPF, @DataNasc);
+
+			DECLARE @CodLogin smallint=(SELECT TOP 1 codLogin FROM tbLogin ORDER BY CodLogin);
+			DECLARE @CodDados smallint=(SELECT TOP 1 CodDados FROM tbDadosComuns ORDER BY CodDados);
+			INSERT INTO tbFuncionario(CodLogin, CodDados, Salario, Cargo)
+			VALUES (@CodLogin, @CodDados, @Salario, @Cargo);
+
+			DECLARE @CodFunc smallint=(SELECT TOP 1 CodFunc FROM tbFuncionario ORDER BY CodFunc);
+
+			INSERT INTO tbProfessor(Idioma, CodFunc)
+			VALUES (@Idioma, @CodFunc);
+		END
+END
+GO
+
+
  /*
 exec sp_insert_periodo null, 1, null, null;
 exec sp_insert_periodo null, 1, 2, null;
