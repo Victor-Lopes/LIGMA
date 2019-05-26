541159bdf21bc8a638fba7839c0ec85629e88e55
Victor Hugo Lopes <42098998+Victor-Lopes@users.noreply.github.com>
2019-05-26 15:57:04 -0300

    Tudo que eu tenho até agora

--- ESaiaDeMansinho/SIGMASQL2.sql
+++ ESaiaDeMansinho/SIGMASQL2.sql
@@ -12,22 +12,14 @@ GO
 USE dbLIGMA;
 GO
 
-CREATE TABLE tbDadosComuns (
+CREATE TABLE tbEndereco (
 	CEP char(8),
 	Numero smallint,
-	Nome varchar(75) not null,
-	CodDados smallint identity(1,1)
-	CONSTRAINT PK_DadosComuns PRIMARY KEY,
+	CONSTRAINT PK_Endereco PRIMARY KEY CLUSTERED(CEP, Numero),
 	Complemento varchar(30) not null,
 	Cidade varchar(25) not null,
 	UF char(2) not null,
-	Logradouro varchar(10) not null,
-	TelFixo decimal(8),
-	TelCelular decimal(9),
-	DataNasc date not null,
-	RG char(10) not null,
-	CPF decimal(11) not null
-	CONSTRAINT UK_Func_CPF UNIQUE(CPF),
+	Logradouro varchar(10) not null
 	);
 GO
 
@@ -35,7 +27,7 @@ CREATE TABLE tbLogin (
 	CodLogin smallint identity(1,1)
 	CONSTRAINT PK_Login PRIMARY KEY,
 	Senha varchar(20) not null,
-	Email varchar(50) not null,
+	Email varchar(20) not null,
 	Ativada bit not null default 1 --1 pra verdadeiro 0 pra falso
 	CONSTRAINT UK_Login_Email UNIQUE
 	);
@@ -44,10 +36,17 @@ GO
 CREATE TABLE tbFuncionario (
 	CodFunc smallint identity(1,1)
 	CONSTRAINT PK_Func PRIMARY KEY,
+	RG char(10) not null,
+	TelFixo decimal(8),
+	CPF decimal(11) not null
+	CONSTRAINT UK_Func_CPF UNIQUE,
+	Nome varchar(75) not null,
 	Salario money not null,
-	Cargo varchar(20) not null,
-	CodDados smallint
-	CONSTRAINT FK_Func_Dados FOREIGN KEY REFERENCES tbDadosComuns(CodDados),
+	DataNasc date not null,
+	TelCelular decimal(9),
+	CEP char(8) not null,
+	Numero smallint not null,
+	CONSTRAINT FK_Func_End FOREIGN KEY(Cep, Numero) REFERENCES tbEndereco(Cep, Numero),
 	CodLogin smallint not null
 	CONSTRAINT FK_Func_Login FOREIGN KEY REFERENCES tbLogin(CodLogin)
 );
@@ -56,7 +55,7 @@ GO
 CREATE TABLE tbProfessor (
 	CodProf smallint identity(1,1)
 	CONSTRAINT PK_Prof PRIMARY KEY,
-	Idioma varchar(18) not null,
+	Idioma varchar(15) not null,
 	CodFunc smallint not null
 	CONSTRAINT FK_Prof_Func FOREIGN KEY REFERENCES tbFuncionario(CodFunc)
 );
@@ -67,24 +66,30 @@ CREATE TABLE tbDiaSemana(
 	CONSTRAINT PK_DiaSemana PRIMARY KEY,
 	Nome varchar(7) not null
 );
+
+insert into tbDiaSemana values('Segunda'),
+							  ('Terça'),
+							  ('Quarta'),
+							  ('Quinta'),
+							  ('Sexta'),
+							  ('Sábado'),
+							  ('Domingo'); 
 GO
 
 CREATE TABLE tbPeriodo(
 	CodPeriodo smallint identity(1,1)
 	CONSTRAINT PK_Periodo PRIMARY KEY,
-	NomePeriodo varchar(7) not null, --3 dias(seg, quarta, sexta), 2 dias(ter, quinta), 1 dia(sabado),
-	HorasAula int not null,
-	HorarioInicial time not null,
-	HorarioFinal time not null
+	NomePeriodo varchar(30) not null -- TIREI DA TABELA PERIODO AS HORAS
 );
 GO
 
 CREATE TABLE tbPeriodo_DiaSemana(
+	CodPeriodoDiaSemana smallint identity(1,1)
+	CONSTRAINT PK_PeriodoDiaSemana PRIMARY KEY,
 	CodPeriodo smallint not null
 	CONSTRAINT FK_PeriodoDiaSemana_Periodo FOREIGN KEY REFERENCES tbPeriodo(CodPeriodo),
 	CodDiaSemana smallint not null
-	CONSTRAINT FK_PeriodoDiaSemana_DiaSemana FOREIGN KEY REFERENCES tbDiaSemana(CodDiaSemana),
-	CONSTRAINT PK_Periodo_DiaSemana PRIMARY KEY CLUSTERED(CodPeriodo, CodDiaSemana)
+	CONSTRAINT FK_PeriodoDiaSemana_DiaSemana FOREIGN KEY REFERENCES tbDiaSemana(CodDiaSemana) --MUDEI A CHAVE
 );
 GO
 
@@ -121,12 +126,20 @@ CREATE TABLE tbAula(
 CREATE TABLE tbAluno(
 	CodAluno smallint identity(1,1)	
 	CONSTRAINT PK_Aluno PRIMARY KEY,
+	DataNasc date not null,
+	Nome varchar(150) not null,
+	CPF decimal(11) not null,
+	CONSTRAINT UK_Aluno_CPF UNIQUE(CPF),
+	RG varchar(10) not null,
 	DataMatricula date not null,
 	DataFinal date not null,
+	TelFixo decimal(10),
+	TelCel decimal(11),
 	CodTurma smallint not null
 	CONSTRAINT FK_Aluno_Turma FOREIGN KEY REFERENCES tbTurma(CodTurma),
-	CodDados smallint
-	CONSTRAINT FK_Aluno_dados FOREIGN KEY REFERENCES tbDadosComuns(CodDados)
+	CEP char(8) not null,
+	Numero smallint not null,
+	CONSTRAINT FK_Aluno_Endereco FOREIGN KEY(CEP, Numero) REFERENCES tbEndereco(CEP,Numero)
 );
 GO
 
